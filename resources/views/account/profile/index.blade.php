b3508e4b6f2b792313a5579fdcf66c0e48523af4
begimov <begimov@gmail.com>
2018-04-20 17:33:13 +0300

    profile update form implemented

--- resources/views/account/profile/index.blade.php
+++ resources/views/account/profile/index.blade.php
@@ -5,7 +5,18 @@
         <div class="card-body">
             <form action="{{ route('account.profile.store') }}" method="POST">
                 {{ csrf_field() }}
-                <button type="submit">Send</button>
+
+                <div class="form-group">
+                    <label for="name" class="control-label">Username</label>
+                    <input type="text" name="name" id="name" class="form-control" value="{{ old('name', auth()->user()->name) }}">
+                </div>
+
+                <div class="form-group">
+                    <label for="email" class="control-label">Email</label>
+                    <input type="text" name="email" id="email" class="form-control" value="{{ old('email', auth()->user()->email) }}">
+                </div>
+
+                <button type="submit" class="btn btn-primary">Submit</button>
             </form>
         </div>
     </div>
