c3b9a5b67189fc129cc8b16b9705e865fe64c92d
begimov <begimov@gmail.com>
2018-04-24 16:39:42 +0300

    Flashing alerts implemented

--- resources/views/layouts/partials/alerts/_alerts.blade.php
+++ resources/views/layouts/partials/alerts/_alerts.blade.php
@@ -1,11 +1,11 @@
 @if (session('success'))
-    @component('layouts.patrials.alerts._alerts_component')
+    @component('layouts.partials.alerts._alerts_component', ['type' => 'success'])
         {{ session('success') }}
     @endcomponent
 @endif
 
 @if (session('error'))
-    @component('layouts.patrials.alerts._alerts_component')
+    @component('layouts.partials.alerts._alerts_component', ['type' => 'danger'])
         {{ session('error') }}
     @endcomponent
 @endif
