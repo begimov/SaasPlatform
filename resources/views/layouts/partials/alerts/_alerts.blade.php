08087756b5849040b439adcce11f40c42ff37f7a
begimov <begimov@gmail.com>
2018-04-24 16:33:55 +0300

    alerts component and blade template partial implemented

--- resources/views/layouts/partials/alerts/_alerts.blade.php
+++ resources/views/layouts/partials/alerts/_alerts.blade.php
@@ -1,1 +1,11 @@
-sdasd
+@if (session('success'))
+    @component('layouts.patrials.alerts._alerts_component')
+        {{ session('success') }}
+    @endcomponent
+@endif
+
+@if (session('error'))
+    @component('layouts.patrials.alerts._alerts_component')
+        {{ session('error') }}
+    @endcomponent
+@endif
