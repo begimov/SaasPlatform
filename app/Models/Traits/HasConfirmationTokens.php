1f1cb9650f153b9f649c1ddc3806ac506f9081c3
begimov <begimov@gmail.com>
2018-04-24 17:53:05 +0300

    HasConfirmationTokens trait implemented

--- app/Models/Traits/HasConfirmationTokens.php
+++ app/Models/Traits/HasConfirmationTokens.php
@@ -2,7 +2,25 @@
 
 namespace App\Models\Traits;
 
+use App\Models\ConfirmationToken;
+
 trait HasConfirmationTokens
 {
-    
+    public function generateConfirmationToken()
+    {
+        $this->confirmationToken()->create([
+            'token' => $token = str_random(200),
+            'expires_at' => $this->getConfirmationTokenExpiry()
+        ]);
+    }
+
+    public function confirmationToken()
+    {
+        return $this->hasOne(ConfirmationToken::class);
+    }
+
+    protected function getConfirmationTokenExpiry()
+    {
+        return $this->freshTimestamp()->addMinutes(10);
+    }
 }
