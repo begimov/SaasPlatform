3909b02c6d9f9507979cac02d4ad645007aa379e
begimov <begimov@gmail.com>
2018-04-24 17:35:58 +0300

    ConfirmationToken class implemented

--- app/Models/ConfirmationToken.php
+++ app/Models/ConfirmationToken.php
@@ -4,7 +4,33 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\User;
+
 class ConfirmationToken extends Model
 {
-    //
+    public $timestamps = false;
+
+    protected $dates = [
+        'expires_at'
+    ];
+
+    protected $fillable = [
+        'token',
+        'expires_at'
+    ];
+
+    public function getRouteKeyName()
+    {
+        return 'token';
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function hasExpired()
+    {
+        return $this->freshTimestamp()->gt($this->expires_at);
+    }
 }
