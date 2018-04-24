509505ef3e7070a3eb67df854e906e34615378be
begimov <begimov@gmail.com>
2018-04-24 16:55:34 +0300

    blade helpers to switch active nav links implemented

--- resources/views/account/layouts/partials/_navigation.blade.php
+++ resources/views/account/layouts/partials/_navigation.blade.php
@@ -1,11 +1,11 @@
 <ul class="nav nav-pills flex-column">
     <li class="nav-item">
-        <a class="nav-link active" href="{{ route('account.index') }}">Overview</a>
+    <a class="nav-link{{ return_if(on_page('account'), ' active') }}" href="{{ route('account.index') }}">Overview</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link" href="{{ route('account.profile.index') }}">Profile</a>
+        <a class="nav-link{{ return_if(on_page('account/profile'), ' active') }}" href="{{ route('account.profile.index') }}">Profile</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link" href="{{ route('account.password.index') }}">Change password</a>
+        <a class="nav-link{{ return_if(on_page('account/password'), ' active') }}" href="{{ route('account.password.index') }}">Change password</a>
     </li>
 </ul>
