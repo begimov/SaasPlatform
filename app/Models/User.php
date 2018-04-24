f2e3dfd2a7de2583cfeae84379915030186170f9
begimov <begimov@gmail.com>
2018-04-24 17:38:42 +0300

    HasConfirmationTokens trait created and pulled in User model class

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,9 +5,11 @@ namespace App\Models;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+use App\Models\Traits\HasConfirmationTokens;
+
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, HasConfirmationTokens;
 
     /**
      * The attributes that are mass assignable.
