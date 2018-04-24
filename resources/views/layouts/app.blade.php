c8a83f13784ff45b91b7ca9a083586ada3273650
begimov <begimov@gmail.com>
2018-04-24 16:26:48 +0300

    alerts partial template created and included in main layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,6 +26,7 @@
 
         <main class="py-4">
             <div class="container">
+                @include('layouts.partials.alerts._alerts')
                 @yield('content')
             </div>
         </main>
