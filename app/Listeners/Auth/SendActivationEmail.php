6a854e4203ae5c4cf93f31d94f812ef98e611c95
begimov <begimov@gmail.com>
2018-04-25 13:04:19 +0300

    SendActivationEmail listener class created

---
+++ app/Listeners/Auth/SendActivationEmail.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Listeners\Auth;
+
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class SendActivationEmail
+{
+    /**
+     * Handle the event.
+     *
+     * @param  object  $event
+     * @return void
+     */
+    public function handle($event)
+    {
+        //
+    }
+}
